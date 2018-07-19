fcca0b1bc4c0649c90146d5de1f3c5e87b9e5736
allaev <34710484+allaev@users.noreply.github.com>
2018-07-19 12:46:47 +0500

    add form, update dependencies

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    const { fields: {title, categories, content}, handleSubmit } = this.props;
+    console.log(title);
     return(
-      <div>Create Form</div>
+      <form onSubmit={handleSubmit}>
+        <h3>Create A New Post</h3>
+        <div className='form-group'>
+          <label>Title</label>
+          <input type='text' className='form-control' />
+        </div>
+        <div className='form-group'>
+          <label>Categories</label>
+          <input type='text' className='form-control' />
+        </div>
+        <div className='form-group'>
+          <label>Content</label>
+          <textarea type='text' className='form-control' />
+        </div>
+        <button type='submit' className='btn btn-primary'>Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostNew',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
