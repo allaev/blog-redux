15a9378b8eb7e3141f877f1520c62fdf00d0caf4
allaev <34710484+allaev@users.noreply.github.com>
2018-09-09 22:53:53 +0500

    Revert "render list of posts in PostsIndex component"
    This reverts commit 86bb1722aac3ced3b60ca27a7faebccc730b1ed9.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -8,32 +7,13 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
-  renderPosts() {
-    return _.map(this.props.posts, post => {
-      return (
-        <li className="list-group-item" key={post.id}>
-          {post.title}
-        </li>
-      )
-    })
-  }
-
   render() {
     return (
       <div>
-        <h3>Posts</h3>
-        <ul className="list-group">
-          {this.renderPosts()}
-        </ul>
+      	Posts Index
       </div>
     );
   }
 }
 
-
-function mapStateToProps(state) {
-  return { posts: state.posts };
-}
-
-export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
-
+export default connect(null, { fetchPosts })(PostsIndex);
