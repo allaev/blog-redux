efd91aec63a1c138f324334ab5cc2befb7a69b67
allaev <34710484+allaev@users.noreply.github.com>
2018-09-09 21:10:27 +0500

    remove app component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
