13a2627a00a74bc755d7909ed524be14e029f88d
allaev <34710484+allaev@users.noreply.github.com>
2018-09-10 13:57:04 +0500

    create validate function

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -38,7 +38,28 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) { //values - object that contains all values that user entered into the form
+  const errors = {};
+
+  //Validate the inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 4 characters"
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories"
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content please"
+  }
+
+  //if we return an empty object from this function Redux thinks that this form is valid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
 
